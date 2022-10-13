9260b724439334ad729a89392ebd46f7da71d4bf
justafolk <avdhut.kamble776@gmail.com>
2022-10-13 23:29:42 +0530

    added nvim & kitty

---
+++ .config/fish/conf.d/nix.fish
@@ -0,0 +1,2 @@
+
+if test -e /home/justafolk/.nix-profile/etc/profile.d/nix.fish; . /home/justafolk/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+
+    # Commands to run in interactive sessions can go here
+end
+set -x PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
+
+alias l="ptls";
+alias ll="ptls -Al";
+alias php="php7"
+
+## Aliases
+# Replace ls with exa
+alias ls='l'
+alias la='ll'
+
